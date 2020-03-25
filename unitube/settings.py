63b260bd21f1a8ed1afb465818112861324e5465
chuk413 <32914259+chuk413@users.noreply.github.com>
2020-03-25 01:57:40 -0700

    Test cas

--- unitube/settings.py
+++ unitube/settings.py
@@ -51,8 +51,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'cas.middleware.CASMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'cas.middleware.CASMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
