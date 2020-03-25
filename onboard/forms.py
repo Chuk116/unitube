d4c04a6e51757500f5432daede1828c6e40b7c66
chuk413 <32914259+chuk413@users.noreply.github.com>
2020-03-25 01:42:27 -0700

    Test signup

--- onboard/forms.py
+++ onboard/forms.py
@@ -11,9 +11,11 @@ class SignUpForm(UserCreationForm):
     def clean(self):
         email = self.cleaned_data.get('email')
         username = self.cleaned_data.get('username')
-        if User.objects.filter(email=email).exists():
+        userEmail = User.objects.filter(email=email)
+        userName = User.objects.filter(username=username)
+        if userEmail:
             self.add_error('email', "This email is already in use!")
-        if User.objects.filter(username=username).exists():
+        if userName:
             self.add_error('username', 'Username is already in use')
         return self.cleaned_data
 
